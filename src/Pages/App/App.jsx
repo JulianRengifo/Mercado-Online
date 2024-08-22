2b1f9414643ebe8e4ac827a85b08482a39eef920
JulianRengifo <jurenco1997@gmail.com>
2024-08-22 15:24:45 -0500

    Enrutamiento con React Router DOM
    - En App.jsx importamos useRoutes y BrowserRoutes
    
    - Creamos una constante que contenga todas las rutas
    
    Enlazamos las rutas, le decimos su path y su componente
    
    En la constante App retornaremos los elementos
    
    Inserta el componente AppRoutes dentro de BrowserRouter

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,22 +1,37 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
-import NotFound from '../NotFound'
 import Signim from '../Signim'
+import NotFound from '../NotFound'
+
 import './App.css'
 
-function App() {
+const AppRoutes = () => {
+  //useRoutes se utiliza para definir rutas
+  let routes = useRoutes([
+    // Define que cuando la URL sea '/', se renderizará el componente Home
+    { path: '/', element: <Home/> },
+    { path: '/MyAccount', element: <MyAccount/> },
+    { path: '/MyOrder', element: <MyOrder/> },
+    { path: '/MyOrders', element: <MyOrders/> },
+    { path: '/Signim', element: <Signim/> },
+    { path: '*', element: <NotFound/> },
+  ])
+
+  return routes
+}
+
+const App =() => {
 
   return (
-    <div className='bg-red-500'>
-      <Home/>
-      <MyAccount/>
-      <MyOrder/>
-      <MyOrders/>
-      <NotFound/>
-      <Signim/>
-    </div>
+    // BrowserRoutes envuelve la aplicación y proporciona el contexto de enrutamiento
+    /*Inserta el componente AppRoutes dentro de BrowserRouter. Esto asegura que el enrutamiento 
+    esté habilitado y que las rutas definidas en AppRoutes funcionen correctamente.*/
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
   )
 }
 
